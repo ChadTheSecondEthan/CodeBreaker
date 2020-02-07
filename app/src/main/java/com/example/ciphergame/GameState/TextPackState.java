8f93a40927dc826334589a64dbb10ffebed90e53
Robotics <martinj1@franklinacademy.org>
2020-02-07 07:16:48 -0500

    Merge branch 'master' of C:\Users\Robotics\StudioProjects\CipherGame with conflicts.

--- app/src/main/java/com/example/ciphergame/GameState/TextPackState.java
+++ app/src/main/java/com/example/ciphergame/GameState/TextPackState.java
@@ -1,43 +1,32 @@
 package com.example.ciphergame.GameState;
 
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import com.example.ciphergame.MainActivity;
+import com.example.ciphergame.Views.BackButton;
 import com.example.ciphergame.Views.Title;
 import com.example.ciphergame.Views.ViewHelper;
 import com.example.ciphergame.R;
-import com.example.ciphergame.Views.VolumeButton;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class TextPackState extends GameState {
 
     // TODO add the names of quote packs, both free and paid for ones
 
     private String[] textPacks = new String[] {
-        "Ancient Philosophy Texts", "Inspirational", "American History", "",
+        "Ancient Philosophy Texts", "Inspirational", "American History", "Religious Scriptures",
             "", "" };
 
     public TextPackState(MainActivity app) { super(app); }
 
     public void init() {
         setContentView(R.layout.text_pack_state);
-        getView(R.id.logo).setVisibility(View.INVISIBLE);
-        getView(R.id.version).setVisibility(View.VISIBLE);
 
-        ViewHelper.setMarginsAsPercentOfScreen(getView(R.id.version), 0, 0, 2, 1);
+        ((Title) app.getView(R.id.title)).init(R.string.textPackTitle);
 
-        Title title = getView(R.id.title);
-        title.init(R.string.textPackTitle);
-        title.setTextSize(32);
-        title.setWidth((int) ViewHelper.percentWidth(85));
-
-        ((VolumeButton) getView(R.id.volume_button)).init(app, ViewHelper.BOTTOM_LEFT);
-
-        Button[] textPackButtons = app.getButtons(new int[] { R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6 });
+        Button[] textPackButtons = new Button[] {
+                app.getView(R.id.button1), app.getView(R.id.button2), app.getView(R.id.button3),
+                app.getView(R.id.button4), app.getView(R.id.button5), app.getView(R.id.button6) };
         for (int i = 0; i < textPackButtons.length; i++) {
             ViewHelper.setWidthAndHeightAsPercentOfScreen(textPackButtons[i], 200 / 3.0, 200 / 9.0);
             textPackButtons[i].setText(textPacks[i]);
@@ -46,18 +35,12 @@ public class TextPackState extends GameState {
             final int num = i;
             textPackButtons[i].setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view) { app.setState(MainActivity.LEVELSTATE).getInLevelState().setTextPack(num); }
+                public void onClick(View view) {
+                    app.getInLevelState().setTextPack(num);
+                    app.setState(MainActivity.LEVELSTATE);
+                }
             });
         }
-        ViewHelper.setMarginTopAndBottomAsPercentOfScreen(getView(R.id.button1), 25 / 3.0, 50 / 3.0);
-        ViewHelper.setHeightAsPercentOfScreen(getView(R.id.textPackView), 77.5);
-
-        final Button creditsButton = getView(R.id.creditsButton);
-        ViewHelper.setWidthAndHeightAsPercentOfScreen(creditsButton, 22.5, 7);
-        creditsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { app.setState(MainActivity.CREDITS); }
-        });
-        ViewHelper.setGetBiggerTouchListener(creditsButton);
+        ViewHelper.setHeightAsPercentOfScreen(getView(R.id.textPackView), 80);
     }
 }
